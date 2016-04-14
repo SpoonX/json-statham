d98d22f73437398dae50a8285ff241a422445c05
RWOverdijk <r.w.overdijk@gmail.com>
2016-04-14 21:44:16 +0200

    feat(project): Added mode (flat / expanded)

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ let fs      = require('fs');
 let expand  = require('./lib/expand');
 let flatten = require('./lib/flatten');
 
+const MODE_FLAT   = 'flat';
+const MODE_NESTED = 'nested';
+const MODES       = [MODE_FLAT, MODE_NESTED];
+
+/**
+ * Kick your JSON's ass, with json-statham's help.
+ */
 class Statham {
-  static fromFile(fileName) {
+
+  /**
+   * Creates a new instance using the data from `fileName`.
+   *
+   * @param {String} fileName
+   * @param {String} [mode]
+   *
+   * @return {Promise}
+   */
+  static fromFile(fileName, mode) {
     return new Promise((resolve, reject) => {
       fs.readFile(fileName, 'utf8', (error, data) => {
         if (error) {
@@ -20,21 +36,85 @@ class Statham {
           return reject(exception);
         }
 
-        resolve(new Statham(parsed));
+        resolve(new Statham(parsed, mode));
       });
     });
   }
 
-  constructor(data) {
+  /**
+   * Constructs a new instance of Statham.
+   *
+   * @param {{}}     data
+   * @param {String} [mode]
+   */
+  constructor(data, mode) {
     this.data = data;
+
+    this.setMode(mode);
   }
 
+  /**
+   * Sets the mode.
+   *
+   * @param {String} [mode] Defaults to nested.
+   *
+   * @throws {Error}
+   */
+  setMode(mode) {
+    mode = mode || MODE_NESTED;
+
+    if (MODES.indexOf(mode) === -1) {
+      throw new Error(
+        `Invalid mode supplied. Must be one of "${MODES.join('" or "')}"`
+      );
+    }
+
+    this.mode = mode;
+  }
+
+  /**
+   * Gets the mode.
+   *
+   * @return {String}
+   */
+  getMode() {
+    return this.mode;
+  }
+
+  /**
+   * Expands flat object to nested object.
+   *
+   * @return {{}}
+   */
   expand() {
-    return expand(this.data);
+    return this.isModeNested() ? this.data : expand(this.data);
   }
 
+  /**
+   * Flattens nested object (dot separated keys).
+   *
+   * @return {{}}
+   */
   flatten() {
-    return flatten(this.data);
+    return this.isModeFlat() ? this.data : flatten(this.data);
+  }
+
+  /**
+   * Returns whether or not mode is flat.
+   *
+   * @return {boolean}
+   */
+  isModeFlat() {
+    return this.mode === MODE_FLAT;
+  }
+
+  /**
+   * Returns whether or not mode is nested.
+   *
+   * @return {boolean}
+   */
+  isModeNested() {
+    return this.mode === MODE_NESTED;
   }
 }
 
