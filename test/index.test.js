7d0f9c44b5eed35b94b0914c1f465b899d11b13e
RWOverdijk <r.w.overdijk@gmail.com>
2016-04-16 12:22:57 +0200

    test(project): Added tests.

--- test/index.test.js
+++ test/index.test.js
@@ -5,106 +5,170 @@ let flat    = require('./resources/flat.json');
 let nested  = require('./resources/nested.json');
 let Statham = require('../index.js').Statham;
 
-let testFlat = new Statham(flat);
-let testNested = new Statham(nested);
-let testEmpty = new Statham({});
+describe('Statham', () => {
+  describe('static .fromFile()', () => {
+    it('Should return a new Statham instance with the data from given file.', done => {
+      Statham.fromFile(__dirname + '/resources/nested.json').then(statham => {
+        assert.instanceOf(statham, Statham, 'Not sure what happened here.');
 
+        done();
+      });
+    });
 
-describe('Statham.fromFile()', () => {
-  // @todo test.
-});
+    it('Should return a new Statham instance with the data from given file and given mode.', done => {
+      let instanceCreationPromises = [
+        Statham.fromFile(__dirname + '/resources/flat.json', Statham.MODE_FLAT),
+        Statham.fromFile(__dirname + '/resources/nested.json', Statham.MODE_NESTED)
+      ];
 
+      Promise.all(instanceCreationPromises).then(results => {
+        assert.strictEqual(results[0].mode, Statham.MODE_FLAT, 'Mode is not flat.');
+        assert.strictEqual(results[1].mode, Statham.MODE_NESTED, 'Mode is not nested.');
 
-describe('constructor', () => {
-  it('Should create a new instance based on the file\'s data (flat).', () => {
-    assert.deepEqual(testFlat.data, flat, 'It did not create a new instance.');
+        done();
+      });
+    });
   });
 
-  it('Should create a new instance based on the file\'s data (nested).', () => {
-    assert.deepEqual(testNested.data, nested, 'It did not create a new instance.');
+  describe('.constructor()', () => {
+    it("Should create a new instance with a flat object.", () => {
+      let statham = new Statham(flat);
+
+      assert.deepEqual(statham.data, flat, 'Object was not assigned properly.');
+    });
+
+    it("Should create a new instance with a nested object.", () => {
+      let statham = new Statham(nested);
+
+      assert.deepEqual(statham.data, nested, 'Object was not assigned properly.');
+    });
+
+    it('Should create a new instance with an empty object.', () => {
+      let statham = new Statham({});
+
+      assert.deepEqual(statham.data, {}, 'Object was not assigned properly.');
+    });
+
+    it('Should expose constant MODE_FLAT.', () => {
+      assert.equal(Statham.MODE_FLAT, 'flat', 'Mode not exposed properly.');
+    });
+
+    it('Should expose constant MODE_NESTED.', () => {
+      assert.equal(Statham.MODE_NESTED, 'nested', 'Mode not exposed properly.');
+    });
+
+    it('Should have mode set as "nested" by default.', () => {
+      let statham = new Statham({});
+
+      assert.strictEqual(statham.mode, Statham.MODE_NESTED, 'Mode does not default to nested.');
+    });
+
+    it('Should accept mode as second argument.', () => {
+      let statham = new Statham({}, Statham.MODE_FLAT);
+
+      assert.strictEqual(statham.mode, 'flat', 'It did not accept or assign mode.');
+    });
   });
 
-  it('Should create a new instance based on an empty object.', () => {
-    assert.deepEqual(testEmpty.data, {}, 'It did not create an empty object.');
+  describe('.setMode()', () => {
+    it('Should have mode set to nested by default.', () => {
+      let statham = new Statham({});
+
+      assert.strictEqual(statham.mode, Statham.MODE_NESTED, 'It did not set mode "nested" by default.');
+    });
+
+    it('Should set mode to flat.', () => {
+      let statham = new Statham({});
+
+      statham.setMode(Statham.MODE_FLAT);
+
+      assert.strictEqual(statham.mode, 'flat', 'It did not set the mode to flat.');
+    });
+
+    it('Should set mode to nested.', () => {
+      let statham = new Statham({});
+
+      statham.setMode(Statham.MODE_NESTED);
+
+      assert.strictEqual(statham.mode, 'nested', 'It did not set the mode to nested.');
+    });
+
+    it('Should return error if mode is invalid.', () => {
+      let statham = new Statham({});
+
+      assert.throws(() => {
+        statham.setMode('single');
+      }, Error);
+    });
   });
 
-  it('Should have mode set as "nested" by default.', () => {
-    assert.strictEqual(testFlat.mode, 'nested', 'Mode is not set.');
+  describe('.getMode()', () => {
+    it('Should return "flat" when getting the mode of a flat object.', () => {
+      let statham = new Statham({}, Statham.MODE_FLAT);
+
+      assert.strictEqual(statham.getMode(), 'flat', 'It did not return what we expected.');
+    });
+
+    it('Should return "nested" when getting the mode of a nested object.', () => {
+      let statham = new Statham({}, Statham.MODE_NESTED);
+
+      assert.strictEqual(statham.getMode(), 'nested', 'It did not return what we expected.');
+    });
   });
 
-  it('Should accept mode as second argument.', () => {
-    let testMode = new Statham(flat, 'flat');
-    assert.strictEqual(testMode.mode, 'flat', 'It did not accept mode.');
-  });
-});
+  describe('.expand()', () => {
+    it('Should expand flat object.', () => {
+      let statham = new Statham(flat, Statham.MODE_FLAT);
 
-describe('setMode()', () => {
-  it('Should have mode set to nested by default.', () => {
-    testFlat.setMode();
-    assert.strictEqual(testFlat.mode, 'nested', 'It did not set mode by default.');
-  });
+      assert.deepEqual(statham.expand(), nested, 'It did not expand object.');
+    });
 
-  it('Should set mode to flat.', () => {
-    testFlat.setMode('flat');
-    assert.strictEqual(testFlat.mode, 'flat', 'It did not set the mode to flat.');
+    it('Should return data untouched if the object is already nested.', () => {
+      let statham = new Statham(nested, Statham.MODE_NESTED);
+
+      assert.strictEqual(statham.expand(), nested, 'It did not return the object.');
+    });
   });
 
-  it('Should set mode to nested.', () => {
-    testNested.setMode('nested');
-    assert.strictEqual(testNested.mode, 'nested', 'It did not set the mode to nested.');
+  describe('.flatten()', () => {
+    it('Should flatten nested object.', () => {
+      let statham = new Statham(nested, Statham.MODE_NESTED);
+
+      assert.deepEqual(statham.flatten(), flat, 'It did not flatten object.');
+    });
+
+    it('Should return data untouched if object is already flat.', () => {
+      let statham = new Statham(flat, Statham.MODE_FLAT);
+
+      assert.strictEqual(statham.flatten(), flat, 'It did not return the object.');
+    });
   });
 
-  it('Should return error if mode is invalid.', () => {
-    // @todo test.
-  });
-});
+  describe('.isModeFlat()', () => {
+    it("Should return `true` if the instance's mode is set to flat.", () => {
+      let statham = new Statham({}, Statham.MODE_FLAT);
 
-describe('getMode()', () => {
-  it('Should return "flat" when getting the mode of a flat object.', () => {
-    assert.strictEqual(testFlat.getMode(), 'flat', 'It did not return what we expected.');
-  });
+      assert.strictEqual(statham.isModeFlat(), true, 'It does not return true.');
+    });
 
-  it('Should return "nested" when getting the mode of a nested object.', () => {
-    assert.strictEqual(testNested.getMode(), 'nested', 'It did not return what we expected.');
-  });
-});
+    it("Should return `false` if the instance's mode is set to nested.", () => {
+      let statham = new Statham({}, Statham.MODE_NESTED);
 
-describe('expand()', () => {
-  it('Should expand flat object.', () => {
-    assert.deepEqual(testFlat.expand(), nested, 'It did not expand object.');
+      assert.strictEqual(statham.isModeFlat(), false, 'It does not return false.');
+    });
   });
 
-  it('Should return itself if the object is already nested.', () => {
-    assert.deepEqual(testNested.expand(), testNested.data, 'It did not return the object.');
-  });
-});
+  describe('.isModeNested()', () => {
+    it("Should return `true` if the object's mode is set to nested.", () => {
+      let statham = new Statham({}, Statham.MODE_NESTED);
 
-describe('flatten()', () => {
-  it('Should flatten nested object.', () => {
-    assert.deepEqual(testNested.flatten(), flat, 'It did not flatten object.');
-  });
+      assert.strictEqual(statham.isModeNested(), true, 'It does not return true.');
+    });
 
-  it('Should return itself if object is already flat.', () => {
-    assert.deepEqual(testFlat.flatten(), testFlat.data, 'It did not return the object.');
-  });
-});
+    it("Should return `false` if the object's mode is set to flat.", () => {
+      let statham = new Statham({}, Statham.MODE_FLAT);
 
-describe('isModeFlat()', () => {
-  it("Should return 'true' if the object's mode is set to flat.", () => {
-    assert.strictEqual(testFlat.isModeFlat(), true, 'It does not return true.');
-  });
-
-  it("Should return 'false' if the object's mode is set to nested.", () => {
-    assert.strictEqual(testNested.isModeFlat(), false, 'It does not return false.');
-  });
-});
-
-describe('isModeNested()', () => {
-  it("Should return 'true' if the object's mode is set to nested.", () => {
-    assert.strictEqual(testNested.isModeNested(), true, 'It does not return true.');
-  });
-
-  it("Should return 'false' if the object's mode is set to flat.", () => {
-    assert.strictEqual(testFlat.isModeNested(), false, 'It does not return false.');
+      assert.strictEqual(statham.isModeNested(), false, 'It does not return true.');
+    });
   });
 });
